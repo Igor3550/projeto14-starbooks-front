7a2031b1fc0579706578010363a5007dd6a63b1a
Igor3550 <igormeloigormelo@gmail.com>
2022-09-14 19:44:44 -0300

    tela de login: criando userContext

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import UserContext from './contexts/UserContext'
 import { Container } from "./styles/appStyle";
 import GlobalStyle from "./styles/GlobalStyle";
 import SignUpPage from "./components/pages/SignUpPage";
 import SignInPage from "./components/pages/SignInPage";
 
 function App() {
+  const [user, setUser] = useState({})
+
   return (
     <Container>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<SignInPage />} />
-          <Route path='/sign-up' element={<SignUpPage />} />
-        </Routes>
-      </BrowserRouter>
+      <UserContext.Provider value={{ user, setUser }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<SignInPage />} />
+            <Route path='/sign-up' element={<SignUpPage />} />
+          </Routes>
+        </BrowserRouter>
+      </UserContext.Provider>
     </Container>
   );
 }
