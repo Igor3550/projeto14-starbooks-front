174071d6b5780a7ca22929935ff0c9046bf686a4
ana-aranha <anab.aranha@gmail.com>
2022-09-15 02:06:21 -0300

    Initial homepage layout

---
+++ src/components/Footer/Footer-style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const FootStyle = styled.div`
+	position: fixed;
+	bottom: 0;
+	width: 100%;
+	height: 40px;
+	background-color: #335470;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+
+	.icons {
+		color: #ffffff;
+		font-size: 26px;
+	}
+`;
